f378fcbb513fe48df25ea8f9edbd4b88908a881e
Nathan Choo <weiwen13@gmail.com>
2024-11-01 03:13:22 +0800

    Made status bar black with white text

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,21 @@
 import { Stack } from "expo-router/stack";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 
 export default function Layout() {
+  const colorScheme = useColorScheme();
+  // console.log("Color scheme:", colorScheme);
   return (
     <>
+      <StatusBar
+        style={colorScheme === "dark" ? "light" : "dark"}
+        backgroundColor={colorScheme === "dark" ? "black" : "white"}
+      />
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="dark" />
     </>
   );
 }
